bd0a20933b66646ea6fe31cf22cd6c9c82d6cc9d
Ultrapyre <sidneymakhiutao@yahoo.com.hk>
2022-09-27 21:39:36 +0800

    Added a functioning button. Actual API functionality can wait.

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 // graphs //
 // currency converter //
 // News //
+var searchButton = document.getElementById("search-button");
+var searchInput = document.getElementById("search-input");
+
+var recentQuery = "";
+
+searchButton.addEventListener("click", function(){
+    query = searchInput.value;
+    if (query&&query!=recentQuery){
+        recentQuery = query;
+        //Insert news update function here
+        console.log("Searched for "+query)
+    }
+    else{
+        console.log("No input detected!");
+    }
+})
