e1793f276ccd8924bad46892a6dc57c02d865cc5
Ultrapyre <sidneymakhiutao@yahoo.com.hk>
2022-09-29 21:54:18 +0800

    Got the World News API to work. Now to hammer it all together into usable data.

--- script.js
+++ script.js
@@ -6,14 +6,36 @@ var searchInput = document.getElementById("search-input");
 
 var recentQuery = "";
 
-searchButton.addEventListener("click", function(){
+searchButton.addEventListener("click", function(event){
+    event.preventDefault();
     query = searchInput.value;
     if (query&&query!=recentQuery){
         recentQuery = query;
         //Insert news update function here
         console.log("Searched for "+query)
+       fetchNews(query)
     }
     else{
         console.log("No input detected!");
     }
 })
+
+function fetchNews(query){
+    var queryURL = "https://api.worldnewsapi.com/search-news?text=currency&number=8&entities=LOC:"+query+"&api-key=aaaebe348fef4f01a1821d7fab811483";
+    
+    fetch(queryURL)
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        generateNews(data);
+    })
+}
+
+function generateNews(data){
+    for(i=0; i<8;i++){
+        var newsArticle = document.createElement("div");
+        
+    }
+}
