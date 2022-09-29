7fa5108af56d54cdfefd63e1ac29a6f7c45acf3f
Ultrapyre <sidneymakhiutao@yahoo.com.hk>
2022-09-29 22:31:17 +0800

    Usable data now displays on site. Issue: TOO MUCH INFO! Need to shrink the article somehow...

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // News //
 var searchButton = document.getElementById("search-button");
 var searchInput = document.getElementById("search-input");
+var newsContainer = document.getElementById("news-display")
 
 var recentQuery = "";
 
@@ -36,6 +37,23 @@ function fetchNews(query){
 function generateNews(data){
     for(i=0; i<8;i++){
         var newsArticle = document.createElement("div");
-        
+        var newsTitle = document.createElement("h2");
+        var newsImage = document.createElement("img");
+        var newsText = document.createElement("p")
+        var newsURL = document.createElement("a")
+
+        newsArticle.className = "news-entry"
+
+        newsTitle.textContent = data.news[i].title;
+        newsImage.src=data.news[i].image;
+        newsText.textContent=data.news[i].text;
+        newsURL.href=data.news[i].url;
+
+        newsArticle.appendChild(newsImage);
+        newsArticle.appendChild(newsTitle);
+        newsArticle.appendChild(newsText);
+        newsArticle.appendChild(newsURL);
+
+        newsContainer.appendChild(newsArticle);
     }
 }
